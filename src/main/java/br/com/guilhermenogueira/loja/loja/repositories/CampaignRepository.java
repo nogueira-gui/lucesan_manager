de36d65fb78c4c6337ecfece8e7e02e64220eea4
GUILHERME N SILVA <gui.r13@hotmail.com>
2022-05-30 23:08:05 -0300

    repository campaign fixed

--- src/main/java/br/com/guilhermenogueira/loja/loja/repositories/CampaignRepository.java
+++ src/main/java/br/com/guilhermenogueira/loja/loja/repositories/CampaignRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CampaignRepository extends JpaRepository<Campaign,Long> {
-    Boolean existsByCode(String code);
+    Boolean existsByName(String name);
 
     @Query(value = "SELECT * FROM CAMPAIGN C WHERE C.ACTIVE=1",nativeQuery = true)
     List<Campaign> findAllByActives();
